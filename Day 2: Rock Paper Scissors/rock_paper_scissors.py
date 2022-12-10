8352992d250e83777281f0066550cd517512bac3
Immanuel Shem <90065532+Crayons001@users.noreply.github.com>
2022-12-10 15:45:32 +0300

    Add Part Two Answer

--- Day 2: Rock Paper Scissors/rock_paper_scissors.py
+++ Day 2: Rock Paper Scissors/rock_paper_scissors.py
@@ -1,25 +1,38 @@
+# THIS IS THE SOLUTION FOR PART TWO
+
 myfile = open("F:\ManuC\strategy_guide.txt", "r");
 points = 0
 total = 0
 
 # Dictionary that dictates the points gained depending on the shape selected
 points_dict = {
-    "X" : 1,
-    "Y" : 2,
-    "Z" : 3
+    "A" : 1,
+    "B" : 2,
+    "C" : 3
 }
 
 lines = myfile.readlines()
+my_shape = " "  # Holds the shape that you play
+
 for line in lines:
-    line = line.replace("\n", "")
-    if line == "A Y" or line == "B Z" or line == "C X":     # winning combinations
+    line = line.replace("\n", "")   # Removing new line characters to make it easier to compare strings
+    # Determining shape you are supposed to play depending on the strategy guide
+    if line == "A Y" or line == "B X" or line == "C Z":
+        my_shape = "A"
+    elif line == "A Z" or line == "B Y" or line == "C X":
+        my_shape = "B"
+    else:
+        my_shape = "C"
+
+    # Determining score based on whether round is won, lost or drawn
+    if line[2] == "Z":
         points = 6
-    elif line == "A X" or line == "B Y" or line == "C Z":   # drawing combinations
+    elif line[2] == "Y":
         points = 3
-    else:                                                   # losing combinations
+    else:
         points = 0
 
-    shape_score = points_dict[line[2]]
+    shape_score = points_dict[my_shape]
     total += points + shape_score
 
 print(f"The total score is {total}")
